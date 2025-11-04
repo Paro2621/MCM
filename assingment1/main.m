458ecaddfea2e0167f807518b6a753c5824a50db
Paro2621 <147939264+Paro2621@users.noreply.github.com>
2025-11-04 20:42:10 +0100

    General revision, minor adjustments
    main and test were changed, I'm considering joining them in a single file

--- assingment1/main.m
+++ assingment1/main.m
@@ -6,26 +6,17 @@ clear, close, clc;
 
 %% 1.1 Angle-axis to rot
 
-function err = testAngleAxisToRot(h, theta)
-    R = AngleAxisToRot(h, theta);
-    [h_test, theta_test] = RotToAngleAxis(R);
-    
-    rho = theta*h;
-    rho_test = h_test*theta_test;
-    err = norm(rho - rho_test);
-end
-
 % --- Q1.2 ---
 h = [1; 0; 0];
 theta = deg2rad(90);
 
-err_rho_q12 = testAngleAxisToRot(h, theta)
+R_q11 = AngleAxisToRot(h, theta)
 
 % --- Q1.3 ---
 h = [0; 0; 1];
 theta = pi/3;
 
-err_rho_q13 = testAngleAxisToRot(h, theta)
+R_q12 = AngleAxisToRot(h, theta)
 
 % --- Q1.4 ---
 rho = [-pi/3; -pi/6; pi/3];
@@ -33,179 +24,113 @@ rho = [-pi/3; -pi/6; pi/3];
 theta = norm(rho);
 h = rho/theta;
 
-err_rho_q14 = testAngleAxisToRot(h, theta)
+R_q13 = AngleAxisToRot(h, theta)
 
 %% 1.2 Rot to angle-axis
 
-function s = testRotToAngleAxis(R)
-    if isRot(R)
-        [h, theta] = RotToAngleAxis(R);
-        R_test = AngleAxisToRot(h, theta);
-        
-        alpha = subspace(R, R_test);
-            % computes the principal angle (in radians) between the column 
-            % spaces of the two argiment matrices
-        
-        s = cos(alpha); % 1 = identical subspace, 0 = orthogonal
-    else
-        disp("R is not a rotation matrix")
-        s = NaN;
-    end
-end
-
-
 % --- Q2.2 ---
 R = [1 0 0 ; 0 0 -1; 0 1 0];
-similarity_q22 = testRotToAngleAxis(R)
-
+[h_q22, theta_q22] = RotToAngleAxis(R)
 
 % --- Q2.3 ---
 R = [0.5 -sqrt(3)/2 0; sqrt(3)/2 0.5 0; 0 0 1];
-similarity_q23 = testRotToAngleAxis(R)
-
+[h_q23, theta_q23] = RotToAngleAxis(R)
 
 % --- Q2.4 ---
 R = [1 0 0; 0 1 0; 0 0 1];
-similarity_q24 = testRotToAngleAxis(R)
-
+[h_q24, theta_q24] = RotToAngleAxis(R)
 
 % --- Q2.5 ---
 R = [-1 0 0; 0 -1 0; 0 0 1];
-similarity_q25 = testRotToAngleAxis(R)
-
+[h_q25, theta_q25] = RotToAngleAxis(R)
 
 % --- Q2.6 ---
 R = [-1 0 0; 0 1 0; 0 0 1];
-similarity_q26 = testRotToAngleAxis(R)
+% the aforedmentioned R is not a rotation matrix
 
 %% 1.3 Euler to rot
 % YPR = [psi; theta; phi];
 
-function err = testYPRToRot(v)
-    R = YPRToRot(v);
-    YPR_test = RotToYPR(R);
-
-    err = norm(v - YPR_test);
-end
-
 % --- Q3.2 ---
 YPR = [0; 0; pi/2];
-err_q32 = testYPRToRot(YPR)
+R_q32 = YPRToRot(YPR)
 
 % --- Q3.3 ---
 YPR = [deg2rad(60); 0; 0];
-err_q33 = testYPRToRot(YPR)
+R_q33 = YPRToRot(YPR)
 
 % --- Q3.4 ---
-YPR = [pi/3; pi/2; pi/4];
-err_q34 = testYPRToRot(YPR)
-
-% since gimbal lock was detected -> plot frame
-R = YPRToRot(YPR);
-
-YPR_test = RotToYPR(R);
-R_test = YPRToRot(YPR_test);
-
-figure, 
-subplot(1,2,1)
-plotFrame(R), hold on
-plotFrame(R_test), grid on, axis equal
+YPR_q34 = [pi/3; pi/2; pi/4];
+R_q34 = YPRToRot(YPR_q34)
+% [!] GIMBAL LOCK DETECTED
 
 % --- Q3.5 ---
-YPR = [0; pi/2; -pi/12];
-err_q35 = testYPRToRot(YPR)
+YPR_q35 = [0; pi/2; -pi/12];
+R_q35 = YPRToRot(YPR_q35)
+% [!] GIMBAL LOCK DETECTED
 
-% since gimbal lock was detected -> plot frame
-R = YPRToRot(YPR);
-
-YPR_test = RotToYPR(R);
-R_test = YPRToRot(YPR_test);
-
-subplot(1,2,2)
-plotFrame(R), hold on
-plotFrame(R_test), grid on, axis equal
-
-% INTERESTING ASPECT: the two results are the same
+% NOTE: R_q35 and R_q34 are the same! 
 % In case of gimbal lock, the first (yaw) and last (roll) rotations both 
 % act about the same axis, so only their sum matters.
-YPR_q34 = [pi/3; pi/2; pi/4];
-YPR_q35 = [0; pi/2; -pi/12];
+YPR_q34_prime = [YPR_q34(1) - YPR_q34(3); pi/2; 0]
+YPR_q35_prime = [YPR_q35(1) - YPR_q35(3); pi/2; 0]
+% so, it's no surprise that the two sets of YPR refer to the same rotation
 
-% thus
-YPR_q34_prime = [YPR_q34(1) - YPR_q34(3); pi/2; 0];
-YPR_q35_prime = [YPR_q35(1) - YPR_q35(3); pi/2; 0];
-
-diff = mod(abs(YPR_q34_prime(1) - YPR_q35_prime(1)), 2*pi);
-    % w/ small negative value (i.e -1e-17) mod returns 6.2831 which is
-    % exacly 2*pi
-
-if kEq(diff, 0)
-    disp("The two rotation matrices are the same")
-end
 %% 1.4 Rot to Euler
-function s = testRotToYPR(R)
-    if isRot(R)
-        YPR = RotToYPR(R);
-        R_test = YPRToRot(YPR);
-        
-        alpha = subspace(R, R_test);
-            % computes the principal angle (in radians) between the column 
-            % spaces of the two argiment matrices
-        
-        s = cos(alpha); % 1 = identical subspace, 0 = orthogonal
-    else
-        disp("R is not a rotation matrix")
-        s = NaN;
-    end
-end
-
 
 % --- Q4.2 ---
 R = [1 0 0; 0 0 -1; 0 1 0];
-err_q42 = testRotToYPR(R)
+YPR_q42 = RotToYPR(R)
 
 % --- Q4.3 ---
 R = [1/2 -sqrt(3)/2 0; sqrt(3)/2 1/2 0; 0 0 1];
-err_q43 = testRotToYPR(R)
+YPR_q43 = RotToYPR(R)
 
 % --- Q4.4 ---
 R = [0          -sqrt(2)/2  sqrt(2)/2; ...
     0.5         sqrt(6)/4   sqrt(6)/4; ...
     -sqrt(3)/2  sqrt(2)/4   sqrt(2)/4];
-err_q44 = testRotToYPR(R)
+
+YPR_q44 = RotToYPR(R)
+% gimbal lock detected
 
 %% 1.5 Rot to angle-axis with eigenvectors
 
 % --- Q5.1 ---
 R = [1 0 0; 0 0 -1; 0 1 0];
 
-[h, theta] = RotToAngleAxis(R)
+[h_q51, theta_q51] = RotToAngleAxis(R)
 
-[V,D] = eigs(R);
-V = real(V);
-D = real(D);
-
-for i = 1:3
-    if D(i, i) == 1
-        h_star = V(:, i)
-    end
-end
-
-theta_star = acos((trace(R)-1)/2)
-
-% --- Q5.2 ---
-R = 1/9 * [4 -4 -7; 8 1 4; -1 -8 4];
-
-[h, theta] = RotToAngleAxis(R)
-
-[V,D] = eigs(R);
+[V,D] = eig(R);
 V = real(V);
 D = real(D);
 
 for i = 1:3
     if kEq(D(i, i), 1)
-        h_star = V(:, i)
+        h_star_q51 = V(:, i)
     end
 end
 
-theta_star = acos((trace(R)-1)/2)
+theta_star_q51 = acos((trace(R)-1)/2)
+
+% --- Q5.2 ---
+R = 1/9 * [4 -4 -7; 8 1 4; -1 -8 4];
+
+[h_q52, theta_q52] = RotToAngleAxis(R)
+
+[V,D] = eig(R);
+V = real(V);
+D = real(D);
+
+for i = 1:3
+    if kEq(D(i, i), 1)
+        h_star_q52 = V(:, i)
+    end
+end
+
+theta_star_q52 = acos((trace(R)-1)/2)
+
+% [!] Attention, since the eigenvector can be scaled for any real lambda,
+% negative and positive, this methods do not make distintion between [-avt]
+% and [+avt], thus, it is not suitable for this applcation w/out additional
+% checks. The use of RotToAngleAxis() is suggested
--- assingment1/test.m
+++ assingment1/test.m
@@ -4,44 +4,208 @@ addpath('utils/plot');
 
 clear, close, clc;
 
-%% TEST 1
-roll = deg2rad(10);
-pitch = deg2rad(30);
-yaw = deg2rad(45);
+%% 1.1 Angle-axis to rot
 
-YPR = [yaw; pitch; roll];
+function err = testAngleAxisToRot(h, theta)
+    R = AngleAxisToRot(h, theta);
+    [h_test, theta_test] = RotToAngleAxis(R);
+    
+    rho = theta*h;
+    rho_test = h_test*theta_test;
+    err = norm(rho - rho_test);
+end
 
-rot = YPRToRot(YPR);
+% --- Q1.2 ---
+h = [1; 0; 0];
+theta = deg2rad(90);
 
-YPR_1 = RotToYPR(rot);
+err_rho_q12 = testAngleAxisToRot(h, theta)
 
-rot_1 = YPRToRot(YPR_1);
+% --- Q1.3 ---
+h = [0; 0; 1];
+theta = pi/3;
 
-% figure, 
-% dispFrame(rot)
-% dispFrame(rot_1)
-% axis equal
+err_rho_q13 = testAngleAxisToRot(h, theta)
 
-delta_test1 = YPR_1 - YPR
+% --- Q1.4 ---
+rho = [-pi/3; -pi/6; pi/3];
 
-%% TEST 2
-roll = deg2rad(10);
-pitch = deg2rad(30);
-yaw = deg2rad(45);
+theta = norm(rho);
+h = rho/theta;
 
-YPR = [yaw; pitch; roll];
+err_rho_q14 = testAngleAxisToRot(h, theta)
 
-rot = YPRToRot(YPR);
+%% 1.2 Rot to angle-axis
 
-[h, theta] = RotToAngleAxis(rot);
+function s = testRotToAngleAxis(R)
+    if isRot(R)
+        [h, theta] = RotToAngleAxis(R);
+        R_test = AngleAxisToRot(h, theta);
+        
+        alpha = subspace(R, R_test);
+            % computes the principal angle (in radians) between the column 
+            % spaces of the two argiment matrices
+        
+        s = cos(alpha); % 1 = identical subspace, 0 = orthogonal
+    else
+        disp("R is not a rotation matrix")
+        s = NaN;
+    end
+end
 
-rot_1 = AngleAxisToRot(h, theta);
 
-YPR_1 = RotToYPR(rot_1);
+% --- Q2.2 ---
+R = [1 0 0 ; 0 0 -1; 0 1 0];
+similarity_q22 = testRotToAngleAxis(R)
 
-% figure, 
-% dispFrame(rot)
-% dispFrame(rot_1)
-% axis equal
 
-delta_test2 = YPR_1 - YPR
+% --- Q2.3 ---
+R = [0.5 -sqrt(3)/2 0; sqrt(3)/2 0.5 0; 0 0 1];
+similarity_q23 = testRotToAngleAxis(R)
+
+
+% --- Q2.4 ---
+R = [1 0 0; 0 1 0; 0 0 1];
+similarity_q24 = testRotToAngleAxis(R)
+
+
+% --- Q2.5 ---
+R = [-1 0 0; 0 -1 0; 0 0 1];
+similarity_q25 = testRotToAngleAxis(R)
+
+
+% --- Q2.6 ---
+R = [-1 0 0; 0 1 0; 0 0 1];
+similarity_q26 = testRotToAngleAxis(R)
+
+%% 1.3 Euler to rot
+% YPR = [psi; theta; phi];
+
+function err = testYPRToRot(v)
+    R = YPRToRot(v);
+    YPR_test = RotToYPR(R);
+
+    err = norm(v - YPR_test);
+end
+
+% --- Q3.2 ---
+YPR = [0; 0; pi/2];
+err_q32 = testYPRToRot(YPR)
+
+% --- Q3.3 ---
+YPR = [deg2rad(60); 0; 0];
+err_q33 = testYPRToRot(YPR)
+
+% --- Q3.4 ---
+YPR = [pi/3; pi/2; pi/4];
+err_q34 = testYPRToRot(YPR)
+
+% since gimbal lock was detected -> plot frame
+R = YPRToRot(YPR);
+
+YPR_test = RotToYPR(R);
+R_test = YPRToRot(YPR_test);
+
+figure, 
+subplot(1,2,1)
+plotFrame(R), hold on
+plotFrame(R_test), grid on, axis equal
+
+% --- Q3.5 ---
+YPR = [0; pi/2; -pi/12];
+err_q35 = testYPRToRot(YPR)
+
+% since gimbal lock was detected -> plot frame
+R = YPRToRot(YPR);
+
+YPR_test = RotToYPR(R);
+R_test = YPRToRot(YPR_test);
+
+subplot(1,2,2)
+plotFrame(R), hold on
+plotFrame(R_test), grid on, axis equal
+
+% INTERESTING ASPECT: the two results are the same
+% In case of gimbal lock, the first (yaw) and last (roll) rotations both 
+% act about the same axis, so only their sum matters.
+YPR_q34 = [pi/3; pi/2; pi/4];
+YPR_q35 = [0; pi/2; -pi/12];
+
+% thus
+YPR_q34_prime = [YPR_q34(1) - YPR_q34(3); pi/2; 0];
+YPR_q35_prime = [YPR_q35(1) - YPR_q35(3); pi/2; 0];
+
+diff = mod(abs(YPR_q34_prime(1) - YPR_q35_prime(1)), 2*pi);
+    % w/ small negative value (i.e -1e-17) mod returns 6.2831 which is
+    % exacly 2*pi
+
+if kEq(diff, 0)
+    disp("The two rotation matrices are the same")
+end
+%% 1.4 Rot to Euler
+function s = testRotToYPR(R)
+    if isRot(R)
+        YPR = RotToYPR(R);
+        R_test = YPRToRot(YPR);
+        
+        alpha = subspace(R, R_test);
+            % computes the principal angle (in radians) between the column 
+            % spaces of the two argiment matrices
+        
+        s = cos(alpha); % 1 = identical subspace, 0 = orthogonal
+    else
+        disp("R is not a rotation matrix")
+        s = NaN;
+    end
+end
+
+
+% --- Q4.2 ---
+R = [1 0 0; 0 0 -1; 0 1 0];
+err_q42 = testRotToYPR(R)
+
+% --- Q4.3 ---
+R = [1/2 -sqrt(3)/2 0; sqrt(3)/2 1/2 0; 0 0 1];
+err_q43 = testRotToYPR(R)
+
+% --- Q4.4 ---
+R = [0          -sqrt(2)/2  sqrt(2)/2; ...
+    0.5         sqrt(6)/4   sqrt(6)/4; ...
+    -sqrt(3)/2  sqrt(2)/4   sqrt(2)/4];
+err_q44 = testRotToYPR(R)
+
+%% 1.5 Rot to angle-axis with eigenvectors
+
+% --- Q5.1 ---
+R = [1 0 0; 0 0 -1; 0 1 0];
+
+[h, theta] = RotToAngleAxis(R)
+
+[V,D] = eig(R);
+V = real(V);
+D = real(D);
+
+for i = 1:3
+    if kEq(D(i, i), 1)
+        h_star = V(:, i)
+    end
+end
+
+theta_star = acos((trace(R)-1)/2)
+
+% --- Q5.2 ---
+R = 1/9 * [4 -4 -7; 8 1 4; -1 -8 4];
+
+[h, theta] = RotToAngleAxis(R)
+
+[V,D] = eig(R);
+V = real(V);
+D = real(D);
+
+for i = 1:3
+    if kEq(D(i, i), 1)
+        h_star = V(:, i)
+    end
+end
+
+theta_star = acos((trace(R)-1)/2)
