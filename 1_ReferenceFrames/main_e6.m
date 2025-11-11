d9ad7eb66efec41d65fb1680cd3928fcc38a0a97
ric <riccardoparotelli@gmail.com>
2025-11-11 10:14:44 +0100

    fixed error in RotToYPR, minor changes

--- 1_ReferenceFrames/main_e6.m
+++ 1_ReferenceFrames/main_e6.m
@@ -48,41 +48,41 @@ z = [0 0 1]';
 
 figure
 To = tFactory([x y z], [0 0 0]');
-plotFrame(To), hold on, axis equal
+plotFrame(To, '<0>'), hold on, axis equal
 
 oTa = tFactory([x y z], [0 0 1.75]');
-plotFrame(To*oTa)
+plotFrame(To*oTa, '<1>')
 
 aTb = tFactory([-x z y], [0 0 0.98]');
-plotFrame(To*oTa*aTb),
+plotFrame(To*oTa*aTb, '<2>'),
 
 bTc = tFactory([-z y x], [1.05 0 0]');
-plotFrame(To*oTa*aTb*bTc),
+plotFrame(To*oTa*aTb*bTc, '<3>'),
 
 c_T_d = tFactory([-z -y -x], [0 1.455 3.265]');
-plotFrame(To*oTa*aTb*bTc*c_T_d),
+plotFrame(To*oTa*aTb*bTc*c_T_d, '<4>'),
 
 dTe = tFactory([-y -z x], [0.35 0 0]');
-plotFrame(To*oTa*aTb*bTc*c_T_d*dTe),
+plotFrame(To*oTa*aTb*bTc*c_T_d*dTe, '<5>'),
 
 eTf = tFactory([z x y], [0 0 3.85]');
-plotFrame(To*oTa*aTb*bTc*c_T_d*dTe*eTf),
+plotFrame(To*oTa*aTb*bTc*c_T_d*dTe*eTf, '<6>'),
 
 fTg = tFactory([y z x], [1.53 0 0]');
-plotFrame(To*oTa*aTb*bTc*c_T_d*dTe*eTf*fTg),
+plotFrame(To*oTa*aTb*bTc*c_T_d*dTe*eTf*fTg, '<7>'),
 view([-14.10 10.69])
 
 %% Geometria in notazione DH
-close all; 
-clear, clc; 
-
-robot = loadrobot("fanucLRMate200ib");
-% showdetails(robot);
-
-config = homeConfiguration(robot);
-
-figure
-show(robot);
+% close all; 
+% clear, clc; 
+% 
+% robot = loadrobot("fanucLRMate200ib");
+% % showdetails(robot);
+% 
+% config = homeConfiguration(robot);
+% 
+% figure
+% show(robot);
 
 
 
